1a4c5ea3478093113df8efc4c718e1a05855bf27
Rgaumond <101813868+Rgaumond@users.noreply.github.com>
2022-12-01 13:49:34 -0500

    Update mongooseConnect.js

--- mongooseConnect.js
+++ mongooseConnect.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
-const connection = "mongodb://127.0.0.1:27017/SEDB";
+const connection = "mongodb://localhost:27017/SEDB";
 //Db connection
 mongoose.connect(connection, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", function (error) {
-        return console.log(error);
+        return console.log("MONGO ERROR:" + error);
     });
 db.once("open", ()=>{
     console.log("Connected to DB")
