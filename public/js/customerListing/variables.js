a951ea66fc69ef98a51cc3402e96f8764098496d
Rgaumond <101813868+Rgaumond@users.noreply.github.com>
2023-01-05 11:58:18 -0500

    HEro

--- public/js/customerListing/variables.js
+++ public/js/customerListing/variables.js
@@ -3,7 +3,8 @@
   $("#header-right").append(
     `<div id="filterContainer"></div>
      <div id='eng-header-action'>
-        <div id='reference-button' class='header-button-blank' onclick='printCards("amount")'>Sort Amount</div>
+        <div id='reference-button' class='header-button-blank' onclick='printCards("amount")'>Sort Amount</div><div id='hero-button' class='header-button-blank' onclick='generateHeroList()'>hero</div>
+        <div class='header-button-blank' onclick='showAddHero()'>hero +</div>
         <div id='reference-button' class='header-button-blank' onclick='addStackDialog()'>stack</div>
         <div id='reference-button' class='header-button-blank' onclick='goToReferences()'>ref</div>
         <div id='toDo-button' class='header-button' onclick='generateToDoList()'></div>
