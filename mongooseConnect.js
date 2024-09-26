c08dfa77f2dfb41a0570353af4c0223743a21467
Rgaumond <101813868+Rgaumond@users.noreply.github.com>
2024-09-26 13:19:53 -0400

    Sept26

--- mongooseConnect.js
+++ mongooseConnect.js
@@ -1,13 +1,17 @@
 const mongoose = require("mongoose");
-const connection = "mongodb://localhost:27017/SEDB";
+const connection =
+  "mongodb+srv://rgaumond:Qk%40qMiJ9TAV%40cv4@cluster0.djc2jgg.mongodb.net/SEDB";
 //Db connection
-mongoose.connect(connection, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(connection, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const db = mongoose.connection;
 db.on("error", function (error) {
-        return console.log("MONGO ERROR:" + error);
-    });
-db.once("open", ()=>{
-    console.log("Connected to DB")
+  return console.log("MONGO ERROR:" + error);
+});
+db.once("open", () => {
+  console.log("Connected to DB");
 });
 
 module.exports = mongoose.connection;
